501b55b43a6a59104a8573453332e6926641f5fa
unknown <vinaykushwah890@gmail.com>
2024-08-17 01:01:21 +0530

    Navbar Create

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import Navbar from './components/Navbar'
+import HomePage from './pages/HomePage'
 
 const App = () => {
   return (
     <div>
+      <Navbar />
+      <HomePage />
 
     </div>
   )
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Herosection from '../components/Herosection'
+
+const HomePage = () => {
+    return (
+        <div>
+            <Herosection />
+        </div>
+    )
+}
+
+export default HomePage
